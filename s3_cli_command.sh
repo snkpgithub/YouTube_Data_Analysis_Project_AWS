b465033fba5a9e047731be26bcd52d48a8bd4e5b
SHASHANK PANDEY <52347492+snkpgithub@users.noreply.github.com>
2024-08-29 20:44:58 -0400

    Update s3_cli_command.sh

--- s3_cli_command.sh
+++ s3_cli_command.sh
@@ -3,16 +3,17 @@
 # To copy all JSON Reference data to same location:
 aws s3 cp . s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
 
-# To copy all data files to its own location, following Hive-style patterns:
-aws s3 cp CAvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=ca/
-aws s3 cp DEvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=de/
-aws s3 cp FRvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=fr/
-aws s3 cp GBvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=gb/
-aws s3 cp INvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=in/
-aws s3 cp JPvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=jp/
-aws s3 cp KRvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=kr/
-aws s3 cp MXvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=mx/
-aws s3 cp RUvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=ru/
-aws s3 cp USvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=us/
+#Run following commands to copy all the data file to its location.
+aws s3 cp CAvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://dataengg-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=us/
 
 
+Note (dataengg-on-youtube-raw-useast1-dev) is my s3 bucket name created in my AWS platform (SHASHANK PANDEY)
